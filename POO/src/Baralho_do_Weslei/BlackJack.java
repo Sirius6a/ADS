2cd48f63c004fd1b77b61764bcbd2995908e6125
Weslei.Dv <oliveiraweslei158@gmail.com>
2026-09-21 17:38:27 -0300

    Mao da casa implementado

--- POO/src/Baralho_do_Weslei/BlackJack.java
+++ POO/src/Baralho_do_Weslei/BlackJack.java
@@ -6,86 +6,134 @@ import java.util.Scanner;
 
 public class BlackJack {
     Baralho deck;
+    ArrayList<Carta> casa;
     ArrayList<Carta> mao;
 
-    BlackJack(){
-        deck = new Baralho();
-        deck.gerar_baralho();
-        deck.embaralhar();
+    public BlackJack() {
+        deck = new Baralho("sim");
+        casa = new ArrayList<>();
         mao = new ArrayList<>();
-        puxar();
-        puxar();
+
+        puxar_mao();
+        puxar_mao();
+        puxar_casa();
+        puxar_casa();
+
         exibir_estado();
         executar();
     }
 
-    void puxar(){
+    void puxar_mao() {
         mao.add(deck.puxar());
     }
 
-    int calcular_pontos(){
+    void puxar_casa() {
+        casa.add(deck.puxar());
+    }
+
+    int calcular_pontos(ArrayList<Carta> cartas) {
         int pontos = 0;
         int num_A = 0;
 
-        for (Carta c: mao){
-            if ((c.valor).equals("A")){
+        for (Carta c : cartas) {
+            if ((c.valor).equals("A")) {
                 num_A++;
-                pontos++;
+                pontos += 1;
             } else if (List.of("J", "Q", "K").contains(c.valor)) {
                 pontos += 10;
             } else {
                 pontos += Integer.parseInt(c.valor);
             }
         }
-        for(int i=0; i<num_A; i++){
-            if (pontos + 10 <= 21){
+
+        for (int i = 0; i < num_A; i++) {
+            if (pontos + 10 <= 21) {
                 pontos += 10;
             }
         }
-
         return pontos;
     }
 
-    void exibir_estado(){
-        int pontos = calcular_pontos();
-        System.out.println("Pontos: "+pontos);
-        System.out.println("Mão: ");
-        System.out.println(mao);
-        if (pontos == 21) { // minha solução foi checar a vitória dentro do exibir_estado
-            System.out.println("Vitória!!!");
-            System.exit(0); // esse código funciona como um breaker
-        }
+    void exibir_estado() {
+        int pontos_mao = calcular_pontos(mao);
+        int pontos_casa = calcular_pontos(casa);
+        System.out.println("\n--- ESTADO DO JOGO ---");
+        System.out.println("Casa: " + casa + " | Pontos: " + pontos_casa);
+        System.out.println("Mão: " + mao + " | Pontos: " + pontos_mao);
+        System.out.println("----------------------\n");
     }
 
-
-    void executar(){
+    void executar() {
         Scanner sc = new Scanner(System.in);
-        int pontos = 0;
-        while (true){
-            System.out.println("Quer continuar?(s/n): ");
+        int pontos_mao = calcular_pontos(mao);
+        int pontos_casa = calcular_pontos(casa);
+
+        while (true) {
+            if (pontos_mao == 21) {
+                System.out.println("Você alcançou 21 pontos! Turno da casa...");
+
+                while (pontos_casa < pontos_mao && pontos_casa <= 21) {
+                    puxar_casa();
+                    pontos_casa = calcular_pontos(casa);
+                    exibir_estado();
+                }
+
+                exibir_resultados_finais(pontos_casa, pontos_mao);
+                break;
+            }
+
+            System.out.print("Quer puxar mais uma?(s/n): ");
             String op = sc.nextLine();
-            if(op.equals("n")){
-                System.out.println("Fim do Jogo");
+
+            if (op.equals("n")) {
+                while (pontos_casa < pontos_mao && pontos_casa <= 21) {
+                    puxar_casa();
+                    pontos_casa = calcular_pontos(casa);
+                    exibir_estado();
+                }
+
+                exibir_resultados_finais(pontos_casa, pontos_mao);
                 break;
             }
-            puxar();
+
+            puxar_mao();
+            pontos_mao = calcular_pontos(mao);
             exibir_estado();
-            pontos = calcular_pontos();
-            if (pontos > 21){
-                System.out.println("Você tem mais que 21. Você Perdeu");
+
+            if (pontos_mao > 21) {
+                System.out.println("Você tem mais que 21. Você Perdeu!");
                 break;
             }
-//            if (pontos == 21){
-//                System.out.println("Vitória!!!");
-//                break;
-//            }
         }
-
-        System.out.println("Pontuação final: "+pontos);
+        Reiniciar();
     }
 
-    static void main(){
-        BlackJack jogo = new BlackJack();
+    // Método auxiliar para evitar repetição de código na hora de mostrar o resultado
+    void exibir_resultados_finais(int pontos_casa, int pontos_mao) {
+        System.out.println("Pontuação final da casa: " + pontos_casa);
+        System.out.println("Pontuação final na mão: " + pontos_mao);
+
+        if (pontos_casa > 21) {
+            System.out.println("A casa estourou! Você venceu!!!");
+        } else if (pontos_casa > pontos_mao) {
+            System.out.println("A casa venceu!");
+        } else {
+            System.out.println("Empate!");
+        }
     }
 
+    void Reiniciar() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("\nQuer jogar novamente?(s/n): ");
+        String rr = s.nextLine();
+        if (rr.equalsIgnoreCase("s")) {
+            new BlackJack();
+        } else {
+            System.out.println("Obrigado por jogar!");
+        }
+    }
+
+    public static void main(String[] args) {
+        new BlackJack();
+    }
 }
